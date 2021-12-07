5c34e53079e55dbaa16bd66c8def4c268718256d
bura1 <burictomislavog@gmail.com>
2021-12-07 18:35:52 +0100

    WS_Posts klasa

--- wavesurfer-posts.php
+++ wavesurfer-posts.php
@@ -10,10 +10,45 @@
  * Author URI: https://trapchords.com
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: ws-posts
+ * Text Domain: wavesurfer-posts
  * Domain Path: /languages
  */
 
-if( !defined( 'ABSPATH' ) ) {
+if(!defined('ABSPATH')) {
     exit;
+}
+
+if(!class_exists('WS_Posts')) {
+    class WS_Posts {
+        function __construct() {
+            $this->define_constants();
+        }
+
+        public function define_constants() {
+            define('WS_POSTS_PATH', plugin_dir_path( __FILE__ ));
+            define('WS_POSTS_URL', plugin_dir_url( __FILE__ ));
+            define('WS_POSTS_VERSION', '1.0.0');
+        }
+
+        public static function activate() {
+            update_option( 'rewrite_rules', '' );
+        }
+
+        public static function deactivate() {
+            flush_rewrite_rules();
+            unregister_post_type('wavesurfer-posts');
+        }
+
+        public static function uninstall() {
+
+        }
+    }
+}
+
+if(class_exists('WS_Posts')) {
+    register_activation_hook(__FILE__, array('WS_Posts', 'activate'));
+    register_deactivation_hook(__FILE__, array('WS_Posts', 'deactivate'));
+    register_uninstall_hook(__FILE__, array('WS_Posts', 'uninstall'));
+
+    $ws_posts = new WS_Posts();
 }
