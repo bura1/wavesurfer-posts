6d46d8f384cf7aa716a15d28109d63fb9ee80571
bura1 <burictomislavog@gmail.com>
2021-12-07 23:52:02 +0100

    Display current values in form

--- views/ws-posts_metabox.php
+++ views/ws-posts_metabox.php
@@ -1,3 +1,8 @@
+<?php
+    $file_name = get_post_meta($post->ID, 'ws_post_audio_name', true);
+    $info = get_post_meta($post->ID, 'ws_post_audio_info', true);
+?>
+
 <table class="form-table wavesurfer_posts-metabox">
     <input type="hidden" name="ws_posts_nonce" value="<?php echo wp_create_nonce("ws_posts_nonce") ?>" >
     <tr>
@@ -5,6 +10,10 @@
             <label for="ws_post_audio_file">Upload file</label>
         </th>
         <td>
+            <?php if ($file_name != "") { ?>
+                <p>Current file is: <strong><?php echo esc_html($file_name); ?></strong></p>
+            <?php } ?>
+
             <input 
                 type="file" 
                 name="ws_post_audio_file" 
@@ -22,7 +31,7 @@
                 type="text" 
                 name="ws_post_audio_info" 
                 id="ws_post_audio_info" 
-                value=""
+                value="<?php echo ($info != "") ? esc_html($info) : ''; ?>"
                 required
             >
         </td>    
