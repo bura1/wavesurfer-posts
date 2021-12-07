b0f1ebb91a7a68105dff1407fcb027669b312d3d
bura1 <burictomislavog@gmail.com>
2021-12-07 18:20:17 +0100

    Created plugin

---
+++ wavesurfer-posts.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Wavesurfer posts
+ * Plugin URI: https://wordpress.org
+ * Description: Custom posts with Wavesurfer
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: TB
+ * Author URI: https://trapchords.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ws-posts
+ * Domain Path: /languages
+ */
+
+if( !defined( 'ABSPATH' ) ) {
+    exit;
+}
